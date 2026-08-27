b359aca00b7cc1634c3ce5b41ce0b815100547b9
Lucas <lucas.gomes@tn3.com.br>
2026-08-27 13:32:42 -0300

    first commit

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{Use: "wl",}
+
+	var cmd = &cobra.Command{
+		Use: "update"
+		Short: "Atualiza aplicacao"
+		Run: func(cmd *cobra.PositionalArgs)
+	}
+
+}
